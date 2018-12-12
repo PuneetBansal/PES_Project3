a2f9cb48a66ec5956ad7e02825a793e1ac3b19d0
PuneetBansal <41087476+PuneetBansal@users.noreply.github.com>
2018-12-11 23:16:35 -0700

    Updating working DMA

--- main.c
+++ main.c
@@ -41,18 +41,45 @@
 
 int16_t dma_buffer[64];
 
+/*typedef struct
+{
+float input;
+float log_output;
+}logval;
+
+logval lookup[]={ {0.05,-1.30}, {0.1,-1}, {0.15,-0.82}, {0.2,-0.70}, {0.25, -0.60},
+						{0.3,-0.52}, {0.35,-0.46}, {0.4,-0.4}, {0.45,-0.34}, {0.50,-0.3},
+						{0.55,-0.26}, {0.60,-0.22}, {0.65,-0.19}, {0.7,-0.15}, {0.75,-0.12},
+						{0.8,-0.10}, {0.85,-0.07}, {0.9,-0.05}, {0.95, -0.02}, {1,0}};
+*/
+
+
+
+
+
 void main()
 {
-	int16_t adc_value_read;
+
+	//int16_t adc_value_read;
+
 	hardware_init();
 	PRINTF("\n\rWelcome to command line");
 	adc_init();
-	ADC0_SC1A = ADC_SC1_ADCH(0); //starting adc
-	PRINTF("\n\n\r%x",dma_buffer);
-	//while(1)
-	{
-	adc_value_read = adc_read();
-	PRINTF("\n\r%d",adc_value_read);
-	}
+	dma_init();
+	//SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK ; /* PORT clock enablement */    LED0_EN; // (PORTA_PCR16 = PORT_PCR_MUX(1))  GPIOA_PDDR |= (1<<16);  LED0_ON;
+
+	PORTE_PCR22 |= PORT_PCR_MUX(1);
+	GPIOE_PDDR  |= 1<<22;
+
+	ADC0_SC1A &= ~(ADC_SC1_ADCH_MASK); //adc start
+	//dma_start();
+	//ADC0_SC1A = ADC_SC1_ADCH(0);     //starting adc
+//	PRINTF("\n\n\r%x",dma_buffer);
+//	while(1)
+//	{
+//	adc_value_read = adc_read();
+//	PRINTF("\n\r%d",adc_value_read);
+//	}
+	dma_start();
 }
 
